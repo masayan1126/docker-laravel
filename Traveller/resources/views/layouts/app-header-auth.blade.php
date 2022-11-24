ca8a670ebb9183f58f14cd7e47b0d1e37b0d4f3d
SerikaShimabukuro <115777771+SerikaShimabukuro@users.noreply.github.com>
2022-11-24 17:27:29 +0900

    フォント変更

--- Traveller/resources/views/layouts/app-header-auth.blade.php
+++ Traveller/resources/views/layouts/app-header-auth.blade.php
@@ -20,7 +20,7 @@
     
     <!-- Styles -->
     <link href="{{ asset('css/app-header-auth.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="../css/login.css">
+    <link rel="stylesheet" href="../css/register.css">
 
 </head>
 <body>
