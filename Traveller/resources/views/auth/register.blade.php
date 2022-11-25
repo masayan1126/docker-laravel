fcb6cd813795eaf7880ecd81d161445d172a3c8d
SerikaShimabukuro <115777771+SerikaShimabukuro@users.noreply.github.com>
2022-11-25 14:23:09 +0900

    icon追加

--- Traveller/resources/views/auth/register.blade.php
+++ Traveller/resources/views/auth/register.blade.php
@@ -67,7 +67,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-primary" <img class="login-logo"src="./css/registericon.png" alt="">
                                     {{ __('Register') }}
                                 </button>
                             </div>
