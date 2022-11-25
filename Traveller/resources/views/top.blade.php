224c29f59c96834fb642bce7d89b9dfef7cad752
SerikaShimabukuro <115777771+SerikaShimabukuro@users.noreply.github.com>
2022-11-25 17:28:59 +0900

    fontリンク追加

--- Traveller/resources/views/top.blade.php
+++ Traveller/resources/views/top.blade.php
@@ -7,6 +7,8 @@
     <title>Document</title>
     <link href="{{ asset('css/top.css') }}" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Itim&display=swap" rel="stylesheet">
 
 </head>
 <body>
