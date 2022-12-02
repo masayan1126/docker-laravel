e6d2f207af1c48a8b93f25e230698754f50252ed
gp4yiii <49759016+gp4yiii@users.noreply.github.com>
2022-12-02 16:44:57 +0800

    ファイル名保存_済
    未　バリデーション
    未　細かい調整
    未　シンボリックリンクの作成
    ーーー
    済　ファイル保存
    済　ファイル名のデータベースへの保存
         User.phpへのfillableの記入忘れ

--- Traveller/app/Http/Controllers/Auth/RegisterController.php
+++ Traveller/app/Http/Controllers/Auth/RegisterController.php
@@ -55,7 +55,7 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
-            // 'image_at' => ['file','max:6144','mimes:jpeg,jpg,png,gif,webp']
+            'image_at' => ['file','max:6144','mimes:jpeg,jpg,png,gif,webp']
         ]);
     }
 
