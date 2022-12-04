f988d473e9bf5da2e5e78eaecba6452a76f89963
iwatahotaka <115777656+iwatahotaka@users.noreply.github.com>
2022-12-04 11:48:20 +0800

    postコントローラー記述完了

--- Traveller/routes/web.php
+++ Traveller/routes/web.php
@@ -27,9 +27,9 @@ Route::get('/top', function () {
     return view('top');
 });
 
-Route::get('/create', function () {
-    return view('create');
-});
+// Route::get('/create', function () {
+//     return view('create');
+// });
 
 Route::get('/edit', function () {
     return view('edit');
@@ -39,9 +39,9 @@ Route::get('/mypage', function () {
     return view('mypage');
 });
 
-Route::get('/post', function () {
-    return view('post');
-});
+// Route::get('/post', function () {
+//     return view('post');
+// });
 
 Route::get('/show', function () {
     return view('show');
@@ -54,3 +54,20 @@ Route::get('/header', function () {
 Route::get('/commentCreate', function () {
     return view('commentCreate');
 });
+
+// 下記にコントローラーを経由したルーティングを記載して下さい
+// 同じURLを指定する場合は上記に記載されたview作成ようの仮ルーティングをコメントアウトして下さい
+
+Route::get('/post', 'PostController@index')->name('posts.index');
+
+Route::get('/post/create', 'PostController@create')->name('posts.create');
+
+Route::post('/post', 'PostController@store')->name('posts.store');
+
+Route::get('/post/{id}', 'PostController@show')->name('posts.show');
+
+Route::get('/post/{id}/edit', 'PostController@edit')->name('posts.edit');
+
+Route::put('/post/{id}', 'PostController@update')->name('posts.update');
+
+Route::delete('/post/{id}', 'PostController@destroy')->name('posts.destroy');
