bd3652412e3b0d3152b313dc6d2f778316fecd98
gp4yiii <49759016+gp4yiii@users.noreply.github.com>
2022-12-07 22:40:52 +0800

    差分画像コミット、シーダー修正
    差分画像コミット
    コピー忘れの画像があった。
    -----
    LikeSeeder
    完全ランダム１５０個だと、重複が出てきてしまう。
    そのため生成ランダムをカウントして個数確認を行い、ない場合のみcreate

--- Traveller/database/seeds/LikeSeeder.php
+++ Traveller/database/seeds/LikeSeeder.php
@@ -13,11 +13,19 @@ class LikeSeeder extends Seeder
      */
     public function run()
     {
-        for ($i=1 ; $i<=150 ; $i++) {
-            Like::create([
-                'user_id' => random_int(1, 5),
-                'post_id' => random_int(1, 15),
-            ]);
+        for ($i=1 ; $i<=70 ; $i++) {
+            $user_id = random_int(1, 5);
+            $post_id = random_int(1, 15);
+            $count = Like::where('user_id', $user_id)->where('post_id', $post_id)->count();
+            $this->command->info("user_id ={$user_id}, post_id={$post_id}");
+            $this->command->info("{$count}");
+
+            if ($count <= 0) {
+                Like::create([
+                    'user_id' => $user_id,
+                    'post_id' => $post_id,
+                ]);
+            }
         }
     }
 }
